d5f610c02bacfdd853499931811d265398303ec9
Daniel Fischer <daniel-fischer@hotmail.de>
2019-02-07 20:05:30 +0100

    init

---
+++ FormsSandbox/MainView.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormsSandbox
+{
+	public partial class MainView : Form
+	{
+		private MainViewModel viewModel;
+
+		public MainView()
+		{
+			InitializeComponent();
+			Start();
+		}
+
+		private void Start()
+		{
+			viewModel = new MainViewModel(new MainModel());
+
+			textBox1.DataBindings.Add("Text", viewModel, "Counter");
+
+			button1.Click += new EventHandler(
+				async (object sender, EventArgs e) 
+				=> await viewModel.IncreaseCounter().ConfigureAwait(true));
+
+			button1.DataBindings.Add("Enabled", viewModel, "CanClick");
+		}
+
+	}
+}
---
+++ FormsSandbox/MainViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FormsSandbox
+{
+	public class MainViewModel : INotifyPropertyChanged
+	{
+		private bool _canClick;
+		private MainModel _model;
+		public MainViewModel(MainModel model)
+		{
+			_model = model;
+			CanClick = true;
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public bool CanClick
+		{
+			get { return _canClick; }
+			set
+			{
+				_canClick = value;
+				NotifyPropertyChanged(nameof(CanClick));
+			}
+		}
+
+		public int Counter
+		{
+			get { return _model.Counter; }
+			private set
+			{
+				_model.Counter = value;
+				NotifyPropertyChanged(nameof(Counter));
+			}
+		}
+		public void Execute(object sender, object parameter)
+		{
+			((ICommand)sender).Execute(parameter);
+		}
+
+		public async Task IncreaseCounter()
+		{
+			CanClick = false;
+			await Task.Run(() => System.Threading.Thread.Sleep(1000));
+			Counter++;
+			CanClick = true;
+		}
+
+		public void NotifyPropertyChanged(string propertyName)
+		{
+			if (PropertyChanged != null)
+				PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
